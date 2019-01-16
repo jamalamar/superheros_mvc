35a58e33213a4c04d8d699f1c11b06ee5ed6e384
Jamal Amar Bahena <jamal@Jamals-MacBook-Pro.local>
2019-01-16 12:06:27 -0600

    ReadfileSync, Readfile, Create read Stream.(created multiple ways to do this)

--- server.js
+++ server.js
@@ -1,9 +1,58 @@
 
+//Using FileSync (not optimal)
+//-----------------------------------------------------------
+// let filestream = require('fs')
+
+// let jsonFile = filestream.readFileSync('superheros.json')
+// 	let superheros = JSON.parse(jsonFile)
+// 	console.log(superheros.join(", "))
+// })
+//-----------------------------------------------------------
+
+
+
+//Using readFile (needs callback
+//-----------------------------------------------------------
+// let filestream = require('fs')
+
+// let jsonFile = filestream.readFile('superheros.json', (error, data)=>{
+// 	let superheros = JSON.parse(data)
+// 	console.log(superheros.join(", "))
+// })
+//-----------------------------------------------------------
+
+
+
+//Using ReadStream
+//-----------------------------------------------------------
 let express = require('express')
+let filestream = require('fs')
+
 let app = express()
 
-app.get('/', (req, res)=> {
-	res.send("Here to save the day...")
+app.get('/', (request, response)=> {
+  let jsonStream = filestream.createReadStream('superheros.json')
+
+  let json = ""
+  jsonStream.on('readable', ()=> {
+    let jsonBuffer = jsonStream.read()
+    if(jsonBuffer != null) {
+      json += jsonBuffer.toString()
+    }
+  })
+
+  jsonStream.on('end', ()=> {
+    response.send(JSON.parse(json).join(", "))
+  })
+
 })
 
-app.listen(3000)
+const PORT = 5000
+app.listen(PORT, ()=> {
+  console.log("Ready! at port " + PORT)
+})
+//-----------------------------------------------------------
+
+
+
+
